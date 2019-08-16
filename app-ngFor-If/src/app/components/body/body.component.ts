d4056b3973809f2b03158cc11734b216256d08be
cristhianA94 <cristhianapolo.94@gmail.om>
2019-08-15 23:08:42 -0500

    app terminada

--- app-ngFor-If/src/app/components/body/body.component.ts
+++ app-ngFor-If/src/app/components/body/body.component.ts
@@ -5,14 +5,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css']
 })
-export class BodyComponent implements OnInit {
+export class BodyComponent {
 
   nombre  = "Cristhian";
   apellido = "Apolo";
   
-  constructor() { }
+  mostrar = true;
 
-  ngOnInit() {
+  frase: any = {
+    mensaje: 'Un gran poder requiere una gran responsabilidad',
+    autor: 'Ben Parker'
   }
+
+  personajes: string[] = ['Kratos', 'Daynerys','Yoshi']
   
 }
