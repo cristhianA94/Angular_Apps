b1b5c57a00706fff6663883b439073da25d11e52
cristhianA94 <cristhianapolo.94@gmail.om>
2019-09-18 02:43:20 -0500

    creando pipes personalizados - finalizando pipes

--- app-pipes/src/app/app.module.ts
+++ app-pipes/src/app/app.module.ts
@@ -8,20 +8,36 @@ import { AppComponent } from './app.component';
 import { BodyComponent } from './components/body/body.component';
 
 /* Angular Material */
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+/* Pipe Fecha Español */
+import { LOCALE_ID } from '@angular/core';
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+
+/* pipe personalizado */
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+
+
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
     AppComponent,
-    BodyComponent
+    BodyComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  //Data Time pipe
+  providers: [{ provide: LOCALE_ID, useValue: 'es' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- app-pipes/src/app/components/body/body.component.ts
+++ app-pipes/src/app/components/body/body.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { reject } from 'q';
 
 
 @Component({
@@ -9,8 +10,36 @@ import { Component } from '@angular/core';
 
 export class BodyComponent {
   texto = "Texto de prueba";
+
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   PI = Math.PI;
+
   a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera C",
+      casa: "19"
+    }
+  }
+
+  valorPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('LLego la data!'), 1500)
+  })
+
+  fecha = new Date();
+
+  text2 = "CriSthIan AnDrÉs ApOlO CeVaLlos";
+
+  video = "0G383538qzQ";
+
+  activar: boolean = true;
+
 }
 
